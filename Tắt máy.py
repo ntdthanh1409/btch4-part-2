ae1f8e9c4a50c5400af78732a7a54d37eb6d73aa
ntdthanh1409 <72120867+ntdthanh1409@users.noreply.github.com>
2020-11-24 20:44:42 +0700

    Update Tắt máy.py

--- Tắt máy.py
+++ Tắt máy.py
@@ -1,6 +1,12 @@
 import os
-check = input("Want to shutdown your computer ? (y/n): ")
-if check == 'n' or check == "N":
-    print("Không muốn tắt máy")
-else:
-    os.system("shutdown /s /t 5")
+import time
+while True :
+    check = input("Bạn muốn tắt máy không ? (y/n): ")
+    if check == 'n' or check == 'N':
+        print('Sẽ hỏi lại sau:')
+        for c in range(1 , 31):
+            print(c, end=' ')
+            time.sleep(1) #Hen gio tat may (30s)
+    else:
+        os.system("shutdown /s /t 1")
+        break
