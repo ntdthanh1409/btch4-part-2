af2f70c3bd0988de52a2525d0a68fdf82212d699
ntdthanh1409 <72120867+ntdthanh1409@users.noreply.github.com>
2020-11-28 20:30:41 +0700

    first commit

---
+++ Sinh ngẫu nhiên 1 Dictionary.py
@@ -0,0 +1,10 @@
+import random
+import string
+name = random.choice(string.ascii_letters)
+m = random.randint(1,200)
+n = random.randint(1,200)
+for i in range(n):
+    name += random.choice(string.ascii_letters)
+dic={'name':name,'age':m}
+print("Số kí tự của name :",n)
+print(dic)
