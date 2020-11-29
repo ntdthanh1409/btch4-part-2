904f5bf6e4ac0376a5c427357261b9e8ce18084a
ntdthanh1409 <72120867+ntdthanh1409@users.noreply.github.com>
2020-11-29 11:30:24 +0700

    Update Sinh ngẫu nhiên 1 Dictionary.py

--- Sinh ngẫu nhiên 1 Dictionary.py
+++ Sinh ngẫu nhiên 1 Dictionary.py
@@ -2,7 +2,7 @@ import random
 import string
 name = random.choice(string.ascii_letters)
 m = random.randint(0,200)
-n = random.randint(0,200)
+n = random.randint(1,200)
 for i in range(n):
     name += random.choice(string.ascii_letters)
 dic={'name':name,'age':m}
