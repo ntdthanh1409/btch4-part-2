b24504f859c66b974bb0ca78ef124f95c0382539
ntdthanh1409 <72120867+ntdthanh1409@users.noreply.github.com>
2020-11-30 23:42:08 +0700

    Update Tìm hiểu thư viện Pandas.py

--- Tìm hiểu thư viện Pandas.py
+++ Tìm hiểu thư viện Pandas.py
@@ -1,3 +1,9 @@
 import pandas as pd
-k = pd.read_csv('so diem.csv')
-print(k)
+k = pd.read_csv('panda.csv')
+print(k.head(10))
+url='https://boris.unibe.ch/113257/15/taxreform.csv'
+data = pd.read_csv(url,sep=';')
+print(data.head(10))
+url='https://api.github.com/users?since=100'
+data = pd.read_json(url)
+print(data.head(10))
