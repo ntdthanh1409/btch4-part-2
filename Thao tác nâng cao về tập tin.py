73da5b21458f829234fff00001d0dd7c970995ae
ntdthanh1409 <72120867+ntdthanh1409@users.noreply.github.com>
2020-12-10 23:56:25 +0700

    Update Thao tác nâng cao về tập tin.py

--- Thao tác nâng cao về tập tin.py
+++ Thao tác nâng cao về tập tin.py
@@ -10,9 +10,9 @@ path = path + input('Tên thư mục bạn muốn tạo:')
 os.mkdir(path)
 os.chdir(path)  
 size = float(input('Nhập dung lượng dữ liệu (1MB-1024MB): '))
-size = size * 1000000 
-n =int(size // 1000000) 
-m =int(size % 1000000)  
+size = size * 1024 * 1024 
+n =int(size // 1024000) 
+m =int(size - n * 1024000)  
 for i in range(1,n):
     k = 'file'+str(i)+'.txt'
     k = open(k, mode= 'w')
