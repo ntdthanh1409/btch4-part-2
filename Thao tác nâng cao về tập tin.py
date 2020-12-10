a4fbf43e07b387cf9952e5ecce35f7db178d55e4
ntdthanh1409 <72120867+ntdthanh1409@users.noreply.github.com>
2020-12-10 23:48:19 +0700

    first commit

---
+++ Thao tác nâng cao về tập tin.py
@@ -0,0 +1,35 @@
+  
+import string , random , os
+def randomstr(): 
+    name = random.choice(string.ascii_letters)
+    for i in range(999999):
+        name += random.choice(string.ascii_letters)
+    return name
+path = input('Nhập đường dẫn nơi bạn muốn tạo thư mục')+'\\'
+path = path + input('Tên thư mục bạn muốn tạo:')
+os.mkdir(path)
+os.chdir(path)  
+size = float(input('Nhập dung lượng dữ liệu (1MB-1024MB): '))
+size = size * 1000000 
+n =int(size // 1000000) 
+m =int(size % 1000000)  
+for i in range(1,n):
+    k = 'file'+str(i)+'.txt'
+    k = open(k, mode= 'w')
+    k.write(randomstr())
+    k.close()
+if m != 0:
+    k = 'file'+ str(n + 1) +'.txt'
+    k = open(f, mode= 'w')
+    name = random.choice(string.ascii_letters)
+    for i in range(m - 1):
+        name += random.choice(string.ascii_letters)
+        k.write(randomstr())
+        k.close()
+print('Đã tạo:', (n+1),'file!')
+
+
+
+
+
+    
