45308e00082e1bfdc87a61b77ef2446c3a1d1bc5
ntdthanh1409 <72120867+ntdthanh1409@users.noreply.github.com>
2021-01-05 11:12:47 +0700

    Update Tìm hiểu thư viện Matplotlib.py

--- Tìm hiểu thư viện Matplotlib.py
+++ Tìm hiểu thư viện Matplotlib.py
@@ -21,6 +21,39 @@ pyplot.bar(years, companyB, color='blue')
 pyplot.legend(["companyA","companyB"])
 pyplot.show()
 #column 
+import matplotlib.pyplot as plt, numpy as np
+
+bt, bt_std = (4, 7, 3, 5, 2), (3, 4, 5, 2, 3)
+btvn, btvn_std = (1, 1, 9, 13, 7), (4, 6, 3, 4, 4)
+ind = np.arange(len(bt))
+width = 0.35
+fig, ax = plt.subplots()
+rects1 = ax.bar(ind - width/2, bt, width, yerr=bt_std,
+                label='Bài tập ',color='#74b9ff')
+rects2 = ax.bar(ind + width/2, btvn, width, yerr=btvn_std,
+                label='BTVN',color='#d63031')
+ax.set_xticks(ind)
+ax.set_xticklabels(('Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5'))
+plt.title('''   Biểu đồ thể hiện so bai tap
+   bài tập lập trình ''')
+plt.xlabel("Thời gian trải qua")
+plt.ylabel("Số bài tập ")
+ax.legend()
+plt.show()
 #box plot
+import matplotlib.pyplot as plt, numpy as np
+x = np.random.randn(30) + np.arange(0, 30)*0.75 + 10
+y = np.random.randn(10) + np.arange(0, 10)*1.0 +20
+z = np.random.randn(10) + np.arange(0, 10)*0.5 + 5
+t = np.random.randn(40) + np.arange(0, 40)*0.4 + 0.0
+
+plt.boxplot([x, y, z, t],
+            labels = ['Mùa Xuân', 'Mùa Hạ', 'Mùa Thu','Mùa Đông'],
+            showfliers = True)
+
+plt.title('''bieu do the hien su bien thien nhiet do''')
+plt.xlabel('Mùa')
+plt.ylabel('Chỉ số nhiệt độ ')
+plt.show()
 
     
