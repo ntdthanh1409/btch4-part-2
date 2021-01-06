74e68f11851d466f3683e5cbf168c57354d18612
ntdthanh1409 <72120867+ntdthanh1409@users.noreply.github.com>
2021-01-07 01:34:29 +0700

    Update Thao tác cơ bản với tập tin và thư mục.py

--- Thao tác cơ bản với tập tin và thư mục.py
+++ Thao tác cơ bản với tập tin và thư mục.py
@@ -1,13 +1,14 @@
 import os
-list1 = []
-list2 = []
-target = 'C:\\'
-for x in os.walk(target):
-    print(x)
-for root, dirname, files in os.walk(target):
-    for x in dirname:
-        list2.append(root + '\\' + x)
-    for x in files:
-        list1.append(root + '\\' + x)
-print("Danh sách tập tin:"+ str(list1))
-print("Danh sách thư mục: "+ str(list2))
+print("tất cả các tệp và thư mục trong ổ C:")
+path = 'C:\\'
+print(os.listdir(path))
+print("")
+
+print("các thư mục:")
+list1 = next(os.walk(path))[1] 
+print(list1)
+print("")
+
+print("các tệp:")
+list2 = next(os.walk(path))[2]
+print(list2)
